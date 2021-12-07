5a405a4250b17f1f903ce611efd30aa8a82e59d3
JAEL-G <jaelopr@gmail.com>
2021-12-07 10:42:02 -0400

    Added various math operations

--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'COMMA DOUBLE INT IS LBRACKET LIST LPAREN NAME RBRACKET RECEIVE RPAREN SHOW STRINGstatement : RECEIVE LPAREN DATA RPARENstatement : SHOW LPAREN DATA RPAREN\n  |  SHOW LPAREN NAME RPARENDATA : INT\n          | DOUBLE\n          | STRING\n          | list\n          | statementterm : DATA\n          | DATA COMMA termlist : LBRACKET term RBRACKETstatement : NAME IS DATAstatement : NAME'
+_lr_signature = 'ADD AND BY COMMA DIVIDE DIV_SYMBOL DOUBLE EXPO_SYMBOL INT IS LBRACKET LIST LPAREN MINUS MINUS_SYMBOL MULTIPLY MULT_SYMBOL NAME PLUS PLUS_SYMBOL RAISE RBRACKET RECEIVE RPAREN SHOW STRING SUBSTRACT TIMES TOstatement : RECEIVE LPAREN data RPARENstatement : ADD data PLUS math_Term\n               | data PLUS_SYMBOL math_Termstatement : SUBSTRACT data MINUS math_Term\n               | data MINUS_SYMBOL math_Termstatement : MULTIPLY data TIMES math_Term\n               | data MULT_SYMBOL math_Termstatement : DIVIDE data BY math_Term\n               | data DIV_SYMBOL math_Termstatement : RAISE data TO math_Term\n               | data EXPO_SYMBOL math_Termmath_Term : datastatement : SHOW LPAREN data RPAREN\n  |  SHOW LPAREN NAME RPARENdata : INT\n          | DOUBLE\n          | STRING\n          | list\n          | statementlist_term : data\n          | data COMMA list_termlist : LBRACKET list_term RBRACKETstatement : NAME IS datastatement : NAME'
     
-_lr_action_items = {'RECEIVE':([0,5,6,7,14,24,],[2,2,2,2,2,2,]),'SHOW':([0,5,6,7,14,24,],[3,3,3,3,3,3,]),'NAME':([0,5,6,7,14,24,],[4,4,16,4,4,4,]),'$end':([1,4,9,10,11,12,13,17,18,21,22,23,],[0,-13,-4,-5,-6,-7,-8,-12,-1,-2,-3,-11,]),'LPAREN':([2,3,],[5,6,]),'IS':([4,16,],[7,7,]),'RPAREN':([4,8,9,10,11,12,13,15,16,17,18,21,22,23,],[-13,18,-4,-5,-6,-7,-8,21,22,-12,-1,-2,-3,-11,]),'COMMA':([4,9,10,11,12,13,17,18,20,21,22,23,],[-13,-4,-5,-6,-7,-8,-12,-1,24,-2,-3,-11,]),'RBRACKET':([4,9,10,11,12,13,17,18,19,20,21,22,23,25,],[-13,-4,-5,-6,-7,-8,-12,-1,23,-9,-2,-3,-11,-10,]),'INT':([5,6,7,14,24,],[9,9,9,9,9,]),'DOUBLE':([5,6,7,14,24,],[10,10,10,10,10,]),'STRING':([5,6,7,14,24,],[11,11,11,11,11,]),'LBRACKET':([5,6,7,14,24,],[14,14,14,14,14,]),}
+_lr_action_items = {'RECEIVE':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,]),'ADD':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,]),'SUBSTRACT':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,]),'MULTIPLY':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,]),'DIVIDE':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,]),'RAISE':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,]),'SHOW':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,]),'NAME':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[10,10,10,10,10,10,10,10,10,10,10,10,10,45,10,10,10,10,10,10,10,]),'INT':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,]),'DOUBLE':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,]),'STRING':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,]),'LBRACKET':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,]),'$end':([1,10,11,12,13,14,23,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,],[0,-24,-15,-16,-17,-18,-19,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'PLUS_SYMBOL':([1,3,10,11,12,13,14,22,23,24,25,26,27,31,32,33,34,35,36,37,38,44,45,46,47,49,50,51,52,53,54,55,56,],[-19,17,-24,-15,-16,-17,-18,17,-19,17,17,17,17,17,17,17,-3,-5,-7,-9,-11,17,-24,17,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'MINUS_SYMBOL':([1,3,10,11,12,13,14,22,23,24,25,26,27,31,32,33,34,35,36,37,38,44,45,46,47,49,50,51,52,53,54,55,56,],[-19,18,-24,-15,-16,-17,-18,18,-19,18,18,18,18,18,18,18,-3,-5,-7,-9,-11,18,-24,18,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'MULT_SYMBOL':([1,3,10,11,12,13,14,22,23,24,25,26,27,31,32,33,34,35,36,37,38,44,45,46,47,49,50,51,52,53,54,55,56,],[-19,19,-24,-15,-16,-17,-18,19,-19,19,19,19,19,19,19,19,-3,-5,-7,-9,-11,19,-24,19,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'DIV_SYMBOL':([1,3,10,11,12,13,14,22,23,24,25,26,27,31,32,33,34,35,36,37,38,44,45,46,47,49,50,51,52,53,54,55,56,],[-19,20,-24,-15,-16,-17,-18,20,-19,20,20,20,20,20,20,20,-3,-5,-7,-9,-11,20,-24,20,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'EXPO_SYMBOL':([1,3,10,11,12,13,14,22,23,24,25,26,27,31,32,33,34,35,36,37,38,44,45,46,47,49,50,51,52,53,54,55,56,],[-19,21,-24,-15,-16,-17,-18,21,-19,21,21,21,21,21,21,21,-3,-5,-7,-9,-11,21,-24,21,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'LPAREN':([2,9,],[16,28,]),'IS':([10,45,],[29,29,]),'PLUS':([10,11,12,13,14,22,23,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,],[-24,-15,-16,-17,-18,39,-19,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'MINUS':([10,11,12,13,14,23,24,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,],[-24,-15,-16,-17,-18,-19,40,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'TIMES':([10,11,12,13,14,23,25,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,],[-24,-15,-16,-17,-18,-19,41,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'BY':([10,11,12,13,14,23,26,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,],[-24,-15,-16,-17,-18,-19,42,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'TO':([10,11,12,13,14,23,27,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,],[-24,-15,-16,-17,-18,-19,43,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'COMMA':([10,11,12,13,14,23,31,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,],[-24,-15,-16,-17,-18,-19,48,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),'RBRACKET':([10,11,12,13,14,23,30,31,33,34,35,36,37,38,46,47,49,50,51,52,53,54,55,56,57,],[-24,-15,-16,-17,-18,-19,47,-20,-12,-3,-5,-7,-9,-11,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,-21,]),'RPAREN':([10,11,12,13,14,23,32,33,34,35,36,37,38,44,45,46,47,49,50,51,52,53,54,55,56,],[-24,-15,-16,-17,-18,-19,49,-12,-3,-5,-7,-9,-11,55,56,-23,-22,-1,-2,-4,-6,-8,-10,-13,-14,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'statement':([0,5,6,7,14,24,],[1,13,13,13,13,13,]),'DATA':([5,6,7,14,24,],[8,15,17,20,20,]),'list':([5,6,7,14,24,],[12,12,12,12,12,]),'term':([14,24,],[19,25,]),}
+_lr_goto_items = {'statement':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[1,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,]),'data':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[3,22,24,25,26,27,31,32,33,33,33,33,33,44,46,33,33,33,33,33,31,]),'list':([0,4,5,6,7,8,15,16,17,18,19,20,21,28,29,39,40,41,42,43,48,],[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,]),'list_term':([15,48,],[30,57,]),'math_Term':([17,18,19,20,21,39,40,41,42,43,],[34,35,36,37,38,50,51,52,53,54,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,17 +27,28 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> statement","S'",1,None,None,None),
-  ('statement -> RECEIVE LPAREN DATA RPAREN','statement',4,'p_receive','tiur.py',44),
-  ('statement -> SHOW LPAREN DATA RPAREN','statement',4,'p_show','tiur.py',50),
-  ('statement -> SHOW LPAREN NAME RPAREN','statement',4,'p_show','tiur.py',51),
-  ('DATA -> INT','DATA',1,'p_data','tiur.py',58),
-  ('DATA -> DOUBLE','DATA',1,'p_data','tiur.py',59),
-  ('DATA -> STRING','DATA',1,'p_data','tiur.py',60),
-  ('DATA -> list','DATA',1,'p_data','tiur.py',61),
-  ('DATA -> statement','DATA',1,'p_data','tiur.py',62),
-  ('term -> DATA','term',1,'p_term','tiur.py',67),
-  ('term -> DATA COMMA term','term',3,'p_term','tiur.py',68),
-  ('list -> LBRACKET term RBRACKET','list',3,'p_list','tiur.py',74),
-  ('statement -> NAME IS DATA','statement',3,'p_is','tiur.py',84),
-  ('statement -> NAME','statement',1,'p_variable','tiur.py',91),
+  ('statement -> RECEIVE LPAREN data RPAREN','statement',4,'p_receive','tiur.py',66),
+  ('statement -> ADD data PLUS math_Term','statement',4,'p_addition','tiur.py',72),
+  ('statement -> data PLUS_SYMBOL math_Term','statement',3,'p_addition','tiur.py',73),
+  ('statement -> SUBSTRACT data MINUS math_Term','statement',4,'p_substraction','tiur.py',85),
+  ('statement -> data MINUS_SYMBOL math_Term','statement',3,'p_substraction','tiur.py',86),
+  ('statement -> MULTIPLY data TIMES math_Term','statement',4,'p_multiplcation','tiur.py',98),
+  ('statement -> data MULT_SYMBOL math_Term','statement',3,'p_multiplcation','tiur.py',99),
+  ('statement -> DIVIDE data BY math_Term','statement',4,'p_division','tiur.py',111),
+  ('statement -> data DIV_SYMBOL math_Term','statement',3,'p_division','tiur.py',112),
+  ('statement -> RAISE data TO math_Term','statement',4,'p_exponent','tiur.py',124),
+  ('statement -> data EXPO_SYMBOL math_Term','statement',3,'p_exponent','tiur.py',125),
+  ('math_Term -> data','math_Term',1,'p_math_Term','tiur.py',137),
+  ('statement -> SHOW LPAREN data RPAREN','statement',4,'p_show','tiur.py',145),
+  ('statement -> SHOW LPAREN NAME RPAREN','statement',4,'p_show','tiur.py',146),
+  ('data -> INT','data',1,'p_data','tiur.py',153),
+  ('data -> DOUBLE','data',1,'p_data','tiur.py',154),
+  ('data -> STRING','data',1,'p_data','tiur.py',155),
+  ('data -> list','data',1,'p_data','tiur.py',156),
+  ('data -> statement','data',1,'p_data','tiur.py',157),
+  ('list_term -> data','list_term',1,'p_list_term','tiur.py',164),
+  ('list_term -> data COMMA list_term','list_term',3,'p_list_term','tiur.py',165),
+  ('list -> LBRACKET list_term RBRACKET','list',3,'p_list','tiur.py',171),
+  ('statement -> NAME IS data','statement',3,'p_is','tiur.py',181),
+  ('statement -> NAME','statement',1,'p_variable','tiur.py',188),
 ]
--- tiur.py
+++ tiur.py
@@ -3,14 +3,26 @@ import ply.yacc as yacc
 from ply.lex import LexToken
 import sys
 TEMP_LIST = []
+
 reserved = {
   'is' : "IS",
   'show' : "SHOW",
   'receive': "RECEIVE",
   'list':'LIST',
+  'and': 'AND',
+  'plus':'PLUS',
+  'add':'ADD',
+  'sub':'SUBSTRACT',
+  'minus':'MINUS',
+  'mult':'MULTIPLY',
+  'times':'TIMES',
+  'div':'DIVIDE',
+  'by':'BY',
+  'raise':'RAISE',
+  'to':'TO',
 }
 
-tokens = ['LPAREN','RPAREN','LBRACKET', 'RBRACKET', 'COMMA', 'STRING', 'NAME', 'DOUBLE', 'INT', ] + list(reserved.values())
+tokens = ['LPAREN','RPAREN','LBRACKET', 'RBRACKET', 'COMMA', 'STRING', 'NAME', 'DOUBLE', 'INT', 'PLUS_SYMBOL','MINUS_SYMBOL','MULT_SYMBOL','DIV_SYMBOL','EXPO_SYMBOL'] + list(reserved.values())
 
 def t_ID(t):
   r'[a-zA-Z_][a-zA-Z_0-9]*'
@@ -25,6 +37,11 @@ t_RBRACKET = r'\]'
 t_COMMA = r'\,'
 t_STRING = r'\".*?\"'
 t_ignore = " \t"
+t_PLUS_SYMBOL = r'\+'
+t_MINUS_SYMBOL = r'\-'
+t_MULT_SYMBOL = r'\*'
+t_DIV_SYMBOL = r'\/'
+t_EXPO_SYMBOL = r'\*\*'
 
 def t_DOUBLE(t):
      r'\d+\.\d+'
@@ -46,13 +63,86 @@ Variables = {}
 
 
 def p_receive(p):
-  '''statement : RECEIVE LPAREN DATA RPAREN'''
+  '''statement : RECEIVE LPAREN data RPAREN'''
   value = input(f"{(p.slice[3].value)}")
   p[0] = value
   return p
 
+def p_addition(p):
+  '''statement : ADD data PLUS math_Term
+               | data PLUS_SYMBOL math_Term'''
+  try:
+    if p.slice[2].value =='+':
+      value = p.slice[1].value + p.slice[3].value
+    else:
+      value = p.slice[2].value + p.slice[4].value
+    p[0]=value
+    return p
+  except:
+    raise("Invalid addition method between data types")
+
+def p_substraction(p):
+  '''statement : SUBSTRACT data MINUS math_Term
+               | data MINUS_SYMBOL math_Term'''
+  try:
+    if p.slice[2].value =='-':
+      value = p.slice[1].value - p.slice[3].value
+    else:
+      value = p.slice[2].value - p.slice[4].value
+    p[0]=value
+    return p
+  except:
+    raise("Invalid substraction method between data types")
+
+def p_multiplcation(p):
+  '''statement : MULTIPLY data TIMES math_Term
+               | data MULT_SYMBOL math_Term'''
+  try:
+    if p.slice[2].value =='*':
+      value = p.slice[1].value * p.slice[3].value
+    else:
+      value = p.slice[2].value * p.slice[4].value
+    p[0]=value
+    return p
+  except:
+    raise("Invalid multiplication method between data types")
+
+def p_division(p):
+  '''statement : DIVIDE data BY math_Term
+               | data DIV_SYMBOL math_Term'''
+  try:
+    if p.slice[2].value =='/':
+      value = p.slice[1].value / p.slice[3].value
+    else:
+      value = p.slice[2].value / p.slice[4].value
+    p[0]=value
+    return p
+  except:
+    raise("Invalid division method between data types")
+
+def p_exponent(p):
+  '''statement : RAISE data TO math_Term
+               | data EXPO_SYMBOL math_Term'''
+  try:
+    if p.slice[2].value =='**':
+      value = p.slice[1].value ** p.slice[3].value
+    else:
+      value = p.slice[2].value ** p.slice[4].value
+    p[0]=value
+    return p
+  except:
+    raise("Invalid division method between data types")
+
+def p_math_Term(p):
+  '''math_Term : data'''
+  p[0]=p[1]
+  return p
+
+
+
+
 def p_show(p):
-  '''statement : SHOW LPAREN DATA RPAREN
+  '''statement : SHOW LPAREN data RPAREN
   |  SHOW LPAREN NAME RPAREN'''
   if p.slice[3].type == 'NAME':
     print(Variables[p.slice[3].value])
@@ -60,7 +150,7 @@ def p_show(p):
     print(p.slice[3].value)
 
 def p_data(p):
-  '''DATA : INT
+  '''data : INT
           | DOUBLE
           | STRING
           | list
@@ -68,15 +158,17 @@ def p_data(p):
   p[0] = p[1]
   return p
 
-def p_term(p):
-  '''term : DATA
-          | DATA COMMA term'''
+
+
+def p_list_term(p):
+  '''list_term : data
+          | data COMMA list_term'''
   global TEMP_LIST
   TEMP_LIST.append(p.slice[1].value)
   return p
 
 def p_list(p):
-  '''list : LBRACKET term RBRACKET'''
+  '''list : LBRACKET list_term RBRACKET'''
   global TEMP_LIST
   COPY = TEMP_LIST.copy()
   COPY.reverse()
@@ -86,7 +178,7 @@ def p_list(p):
 
 
 def p_is(p):
-  '''statement : NAME IS DATA'''
+  '''statement : NAME IS data'''
 
   Variables[p[1]] = p.slice[3].value
   # print(Variables)
